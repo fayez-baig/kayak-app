3e01615fb54f96ea8a8baef9dcdaa3f0bd961c35
fayez-baig <coderninja16@gmail.com>
2022-03-06 20:04:50 +0530

    upated code

--- src/utils/loadable.tsx
+++ src/utils/loadable.tsx
@@ -1,28 +1,35 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import { SuspenseLoader } from 'app/domains/Common/components/loaders';
-import { ReactElement, lazy, Suspense, ReactNode, ComponentType, ComponentProps } from 'react';
+import { SuspenseLoader } from "app/domains/Common/components/loaders";
+import {
+  ReactElement,
+  lazy,
+  Suspense,
+  ReactNode,
+  ComponentType,
+  ComponentProps,
+} from "react";
 
 interface Opts {
-    fallback: ReactNode;
+  fallback: ReactNode;
 }
 type Unpromisify<T> = T extends Promise<infer P> ? P : never;
 
 export const lazyLoad = <T extends Promise<any>, U extends ComponentType<any>>(
-    importFunc: () => T,
-    selectorFunc?: (s: Unpromisify<T>) => U,
-    opts: Opts = { fallback: <SuspenseLoader /> },
+  importFunc: () => T,
+  selectorFunc?: (s: Unpromisify<T>) => U,
+  opts: Opts = { fallback: <SuspenseLoader /> }
 ) => {
-    let lazyFactory: () => Promise<{ default: U }> = importFunc;
+  let lazyFactory: () => Promise<{ default: U }> = importFunc;
 
-    if (selectorFunc) {
-        lazyFactory = () => importFunc().then((module) => ({ default: selectorFunc(module) }));
-    }
+  if (selectorFunc) {
+    lazyFactory = () =>
+      importFunc().then((module) => ({ default: selectorFunc(module) }));
+  }
 
-    const LazyComponent = lazy(lazyFactory);
+  const LazyComponent = lazy(lazyFactory);
 
-    return (props: ComponentProps<U>): ReactElement => (
-        <Suspense fallback={opts.fallback!}>
-            <LazyComponent {...props} />
-        </Suspense>
-    );
+  return (props: ComponentProps<U>): ReactElement => (
+    <Suspense fallback={opts.fallback!}>
+      <LazyComponent {...props} />
+    </Suspense>
+  );
 };
